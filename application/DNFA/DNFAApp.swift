25b18cf1e2afd6514cc56febf5fbbc6ef4dfdc54
Hans32 <nothanscydia@gmail.com>
2026-08-18 07:31:34 +0800

    fix: remove embedded git repo and track app normally

---
+++ application/DNFA/DNFAApp.swift
@@ -0,0 +1,17 @@
+//
+//  DNFAApp.swift
+//  DNFA
+//
+//  Created by Hans Camacho on 8/17/26.
+//
+
+import SwiftUI
+
+@main
+struct DNFAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
